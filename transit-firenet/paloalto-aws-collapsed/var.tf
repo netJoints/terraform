3d064dc342a6d3cb61614393c9ff35db823e6432
Shahzad Ali <52764993+netJoints@users.noreply.github.com>
2021-09-20 16:52:57 -0700

    Create var.tf

---
+++ transit-firenet/paloalto-aws-collapsed/var.tf
@@ -0,0 +1,86 @@
+//Controller and Generic VARIABLES
+
+variable "username_controller" {
+  type        = string
+  description = "Aviatrix Controller's Username"
+  default     = "admin"
+}
+
+variable password_controller {
+  description = "Aviatrix Controller's Password"
+}
+
+variable "controller_ip" {
+  description = "Aviatrix Controller's IP Address"
+}
+
+variable "aws_account_name" {
+  type        = string
+  description = "AWS Account Name"
+}
+
+variable "aws_region" {
+  description = "Pick AWS Region. Default is Oregon"
+  default     = "us-west-2"
+}
+
+// VPC Variables
+
+variable "firenet_vpc_name" {
+  description = "Transit FireNet VPC Name"
+  default     = "AVX-TR-FNET1-VPC"
+}
+
+variable "transit_firenet_cidr" {
+  description = "Transit FireNet CIDR"
+  default     = "10.11.0.0/16"
+}
+
+variable "spoke1_vpc_name" {
+  description = "Spoke 1 VPC Name"
+  default     = "AVX-SPK1-FNET1-VPC"
+}
+
+variable "spoke1_cidr" {
+  description = "Spoke 1 (PROD) CIDR"
+  default     = "10.12.0.0/16"
+}
+
+
+variable "spoke2_vpc_name" {
+  description = "Spoke 2 VPC Name"
+  default     = "AVX-DEV1-FNET1-VPC"
+}
+
+variable "spoke2_cidr" {
+  description = "Spoke 2 (DEV) CIDR"
+  default     = "10.13.0.0/16"
+}
+
+
+//Transit FireNet Variables
+
+variable "transit_firenet_gw_name" {
+  description = "Transit FireNet Gateway Name"
+  default     = "TR-FNET1-GWLB"
+}
+
+
+variable "PAN_firewall" {
+  description = "Firewall Instance Name"
+  default     = "GWLB-PAN-TRFNET"
+}
+
+
+// Spokes Variables
+
+variable "spoke1_gw_name" {
+  description = "Spoke1 Gateway Name"
+  default     = "PROD1-SPK-GWLB"
+}
+
+
+variable "spoke2_gw_name" {
+  description = "Spoke 2 Gateway Name"
+  default     = "DEV1-SPK-GWLB"
+}
