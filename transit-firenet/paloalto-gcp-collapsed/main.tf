44bf95e0f546ff6489e2d0e3de3bb6b18dd1c0c4
Shahzad Ali <52764993+netJoints@users.noreply.github.com>
2022-07-06 11:33:37 -0700

    Update main.tf

--- transit-firenet/paloalto-gcp-collapsed/main.tf
+++ transit-firenet/paloalto-gcp-collapsed/main.tf
@@ -1,30 +1,46 @@
-
-
-
-module "transit_ha_gcp" {
-  source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
-  version = "2.1.3"
-
-  cloud                  = "gcp"
-  name                   = "transit-ha-gcp"
-  region                 = "us-east1"
-  cidr                   = "10.2.0.0/23"
-  account                = "shahzad-gcp"
-  enable_transit_firenet = true
-  lan_cidr               = "10.102.0.0/24"
+provider "aviatrix" {
+  controller_ip           = controller.netjoints.com
+  username                = admin
+  password                = Password1
+  skip_version_validation = false
 }
 
-
-  
-  
-module "mc_firenet_ha_gcp" {
-  source  = "terraform-aviatrix-modules/mc-firenet/aviatrix"
-  version = "1.1.1"
-
-  transit_module = module.transit_ha_gcp
-  firewall_image = "Palo Alto Networks VM-Series Next-Generation Firewall BUNDLE1"
-  egress_enabled = true
-  egress_cidr    = "10.102.1.0/24"
-  mgmt_cidr      = "10.102.3.0/24"
+module "gcp_transit_firenet" {
+  source                  = "terraform-aviatrix-modules/gcp-transit-firenet/aviatrix"
+  version                 = "1.1.0"
+  account                 = "Shahzad-GCP"
+  transit_cidr            = "10.0.0.0/24" 
+  firewall_cidr           = "10.0.1.0/26"
+  region                  = "us-central-1"
+  firewall_image          = "Palo Alto Networks VM-Series Next-Generation Firewall BYOL~9.1.3"
 }
+  
+  
+
+module "gcp-spoke1" {
+  source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
+  version = "1.2.4"
+
+  cloud           = "gcp"
+  name            = "gcp-spoke1"
+  cidr            = 10.1.100.0/24
+  region          = "us-central-1"
+  account         = "Shahzad-GCP"
+  transit_gw      = module.framework.transit["transit1"].transit_gateway.gw_name
+  security_domain = "blue"
+}
+
+module "gcp-spoke2" {
+  source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
+  version = "1.2.4"
+
+  cloud           = "gcp"
+  name            = "gcp-spoke1"
+  cidr            = 10.2.100.0/24
+  region          = "us-central-1"
+  account         = "Shahzad-GCP"
+  transit_gw      = module.framework.transit["transit1"].transit_gateway.gw_name
+  security_domain = "blue"
+}
+
 
