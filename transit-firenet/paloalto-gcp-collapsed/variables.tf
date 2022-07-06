e479d1330a667c95fec08100b9b69cbe78b12db8
Shahzad Ali <52764993+netJoints@users.noreply.github.com>
2022-07-06 11:36:54 -0700

    Create variables.tf

---
+++ transit-firenet/paloalto-gcp-collapsed/variables.tf
@@ -0,0 +1,29 @@
+# username, password and controller_ip is configured in terraform cloud variable section
+# this will keep these secure and not part of github code
+
+ variable "username" {
+  type        = string
+  description = "Aviatrix Controller's Username"
+  default     = ""
+ }
+
+ variable password {
+  description = "Aviatrix Controller's Password"
+  default = ""
+ }
+
+ variable "controller_ip" {
+  description = "Aviatrix Controller's IP Address"
+  default = ""
+ }
+
+variable "gcp_project_name" {
+  type        = string
+  description = "GCP Project Name"
+  default = ""
+}
+
+variable "gcp_region" {
+  description = "Pick GCP Region"
+  default     = "us-west-2"
+}
