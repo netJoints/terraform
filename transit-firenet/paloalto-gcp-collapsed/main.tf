d7c7d03502cbeea31391ec360e3b00b90d0fc9cf
Shahzad Ali <52764993+netJoints@users.noreply.github.com>
2022-07-06 17:16:48 -0700

    Update main.tf

--- transit-firenet/paloalto-gcp-collapsed/main.tf
+++ transit-firenet/paloalto-gcp-collapsed/main.tf
@@ -9,10 +9,14 @@ terraform {
       source = "AviatrixSystems/aviatrix"
       version = "~> 2.22.1"
     }
+    
+    google = {
+      source  = "hashicorp/google"
+      version = "4.14.0"
+    }
   }
   required_version = ">= 1.0.0"
 }
-
 
 
 # Specify Aviatrix as the provider with these parameters:
@@ -25,29 +29,35 @@ provider "aviatrix" {
     controller_ip = var.controller_ip
     username = var.username
     password = var.password
-   # skip_version_validation = false
+    skip_version_validation = true
+    verify_ssl_certificate  = false
 }
 
-module "mc-transit" {
-  source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
-  version = "v2.1.3"
-
-  cloud                  = "GCP"
-  cidr                   = "10.1.0.0/23"
-  region                 = "us-central1"
-  account                = "shahzad-gcp"
-  name                   = "gcp-transit-gw"
+module "mc_transit" {
+  source                 = "terraform-aviatrix-modules/mc-transit/aviatrix"
+  version                = "v2.0.0"
+  cloud                  = var.cloud
+  cidr                   = var.vpcs["firenet"]
+  region                 = var.region
+  account                = var.account
   enable_transit_firenet = true
+  lan_cidr = var.vpcs["lan"]
 }
 
-module "firenet1" {
-  source  = "terraform-aviatrix-modules/mc-firenet/aviatrix"
-  version = "v1.1.1"
-
-  transit_module = module.mc-transit
-  firewall_image = "Palo Alto Networks VM-Series Next-Generation Firewall Bundle 1"
+module "firenet_1" {
+  source                  = "terraform-aviatrix-modules/mc-firenet/aviatrix"
+  version                 = "1.0.0"
+  transit_module          = module.mc_transit
+  firewall_image          = var.firewall_image
+  firewall_image_version  = var.firewall_image_version
+  bootstrap_bucket_name_1 = var.storage_bucket_name
+  egress_cidr             = var.vpcs["egress"]
+  egress_enabled          = true
+  inspection_enabled      = true
+  instance_size           = var.instance_size
+  mgmt_cidr               = var.vpcs["mgmt"]
+  password                = var.password
 }
-
   
 
 module "gcp-spoke1" {
