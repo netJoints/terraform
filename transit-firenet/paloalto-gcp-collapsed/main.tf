c0dee58d80c168432460553fd086fca7d918874b
Shahzad Ali <52764993+netJoints@users.noreply.github.com>
2022-07-06 22:17:28 -0700

    Update main.tf

--- transit-firenet/paloalto-gcp-collapsed/main.tf
+++ transit-firenet/paloalto-gcp-collapsed/main.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     aviatrix = {
       source = "AviatrixSystems/aviatrix"
-      version = "2.21.1-6.6.ga"
+      version = "~>2.21.2"
     }
   }
   required_version = ">= 1.0.0"
